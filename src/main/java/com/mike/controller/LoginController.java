4b3afcbe48f0c4595fd945b63bdf2ba09c72f08d
Mike Ottinger <mikeottinger@gmail.com>
2013-01-06 20:49:52 -0800

    more tweaks for security

---
+++ src/main/java/com/mike/controller/LoginController.java
@@ -0,0 +1,30 @@
+package com.mike.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles the displaying and form submission of the login form
+ * <p/>
+ * User: mike
+ * Date: 1/6/13
+ * Time: 5:06 PM
+ */
+@Controller
+public class LoginController
+{
+    private static final Logger log = LoggerFactory.getLogger(LoginController.class);
+
+//    @RequestMapping(value = "/login", method = RequestMethod.GET)
+//    public String showForm()
+//    {
+//        log.debug("Did this get invoked??? Not sure.");
+//
+//        return "login";
+//    }
+
+    //public
+}
