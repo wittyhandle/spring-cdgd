da7ef6ddecc525047ae3b1c35c86b700dd439935
Mike Ottinger <mikeottinger@gmail.com>
2013-01-21 12:09:08 -0800

    add equals and hashCode methods to the Foo class

--- src/main/java/com/mike/domain/Foo.java
+++ src/main/java/com/mike/domain/Foo.java
@@ -41,4 +41,38 @@ public class Foo
     {
         this.name = name;
     }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
+
+        Foo foo = (Foo) o;
+
+        if (id != null ? !id.equals(foo.id) : foo.id != null)
+        {
+            return false;
+        }
+        if (name != null ? !name.equals(foo.name) : foo.name != null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
 }
