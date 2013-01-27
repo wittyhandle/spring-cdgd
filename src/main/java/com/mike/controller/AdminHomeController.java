22685eee17e1a5e2f07cbea61ce65758988d1068
Mike Ottinger <mikeottinger@gmail.com>
2013-01-26 18:20:39 -0800

    the beginnings of the admin and base controllers

---
+++ src/main/java/com/mike/controller/AdminHomeController.java
@@ -0,0 +1,12 @@
+package com.mike.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * <p/>
+ * User: mike
+ * Date: 1/26/13
+ * Time: 6:02 PM
+ */
+public class AdminHomeController
+{
+}
---
+++ src/main/java/com/mike/controller/BaseAdminController.java
@@ -0,0 +1,12 @@
+package com.mike.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * <p/>
+ * User: mike
+ * Date: 1/26/13
+ * Time: 5:39 PM
+ */
+public class BaseAdminController
+{
+}
