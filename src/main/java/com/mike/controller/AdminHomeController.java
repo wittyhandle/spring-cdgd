135350a2afb68de1f21194d762f552281850b837
Mike Ottinger <mikeottinger@gmail.com>
2013-01-26 18:25:07 -0800

    more work on admin controller

--- src/main/java/com/mike/controller/AdminHomeController.java
+++ src/main/java/com/mike/controller/AdminHomeController.java
@@ -1,5 +1,10 @@
 package com.mike.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created with IntelliJ IDEA.
  * <p/>
@@ -7,6 +12,19 @@ package com.mike.controller;
  * Date: 1/26/13
  * Time: 6:02 PM
  */
-public class AdminHomeController
+@Controller
+@RequestMapping("/admin")
+public class AdminHomeController extends BaseAdminController
 {
+    private static final Logger log = LoggerFactory.getLogger(AdminHomeController.class);
+
+    @RequestMapping("home")
+    public String listProjects()
+    {
+        log.debug("Not much in here for now.");
+
+        return "admin";
+    }
+
+
 }
--- src/main/java/com/mike/controller/BaseAdminController.java
+++ src/main/java/com/mike/controller/BaseAdminController.java
@@ -1,12 +1,29 @@
 package com.mike.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.Collection;
+
 /**
- * Created with IntelliJ IDEA.
+ * Controllers within the admin section extend this to leverage functionality for retrieving the currently logged in
+ * user.
  * <p/>
  * User: mike
  * Date: 1/26/13
  * Time: 5:39 PM
  */
+@Controller
 public class BaseAdminController
 {
+    private static final Logger log = LoggerFactory.getLogger(BaseAdminController.class);
+
+    @ModelAttribute("username")
+    public String retrieveUsername()
+    {
+        log.debug("The retrieveUsername method was called, returning 'freddy' for now.");
+        return "freddy";
+    }
 }
