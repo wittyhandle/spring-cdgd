848258bfae6d9ac52a69e81f7590b7f19276bccd
Mike Ottinger <mikeottinger@gmail.com>
2013-02-02 16:27:19 -0800

    remove references to foo in the indexcontrollertest

--- src/test/java/com/mike/controller/IndexControllerTest.java
+++ src/test/java/com/mike/controller/IndexControllerTest.java
@@ -1,16 +1,12 @@
 package com.mike.controller;
 
-import com.mike.domain.Foo;
-import com.mike.service.FooService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.mock.web.MockHttpServletRequest;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Test the indexController.
@@ -25,22 +21,13 @@ public class IndexControllerTest
     @InjectMocks
     private IndexController indexController = new IndexController();
 
-    @Mock
-    private FooService fooService;
-
     @Test
     public void testIdFound() throws Exception
     {
-        Foo foo = new Foo();
-        foo.setId(0L);
-        foo.setName("fred");
-        when(fooService.getFooById(0l)).thenReturn(foo);
-
         MockHttpServletRequest request = new MockHttpServletRequest();
-        request.setParameter("id", "0");
 
-        indexController.test(request);
+        String result = indexController.test(request);
 
-        verify(fooService).getFooById(0L);
+        assertEquals("index", result);
     }
 }
