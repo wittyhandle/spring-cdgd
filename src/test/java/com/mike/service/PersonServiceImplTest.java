91e75aab790688f995853827e98bcddbd244883f
Mike Ottinger <mikeottinger@gmail.com>
2013-03-12 20:23:52 -0700

    added more unit tests for the new services

---
+++ src/test/java/com/mike/service/PersonServiceImplTest.java
@@ -0,0 +1,40 @@
+package com.mike.service;
+
+import com.mike.dao.ProjectDao;
+import com.mike.domain.Project;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created with IntelliJ IDEA.
+ * <p/>
+ * User: mike
+ * Date: 3/12/13
+ * Time: 8:18 PM
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class PersonServiceImplTest
+{
+    @InjectMocks
+    private PersonServiceImpl personService = new PersonServiceImpl();
+
+    @Mock
+    private ProjectDao projectDao;
+
+    @Test
+    public void testSaveProject() throws Exception
+    {
+        Project project = new Project();
+        project.setName("Jim");
+        project.setDescription("Desc");
+
+        personService.saveProject(project);
+
+        verify(projectDao).persist(project);
+    }
+}
