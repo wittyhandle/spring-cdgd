01f6083ac73164b4a8e2483dde7ba91a57e30460
Mike Ottinger <mikeottinger@gmail.com>
2013-03-21 21:39:34 -0700

    fixing unit tests

--- src/test/java/com/mike/controller/AdminProjectControllerTest.java
+++ src/test/java/com/mike/controller/AdminProjectControllerTest.java
@@ -37,7 +37,7 @@ public class AdminProjectControllerTest
     {
         String result = adminProjectController.listProjects();
 
-        assertEquals("admisn", result);
+        assertEquals("admin", result);
     }
 
     @Test
